634c8c8357b5da103798924a7495a548f158e4ac
Tommy Son <tommy.m.son@gmail.com>
2025-03-30 13:02:26 -0700

    Objects everywhere

--- pragmatic_studio/studio_game/studio_game.rb
+++ pragmatic_studio/studio_game/studio_game.rb
@@ -1,9 +1,9 @@
 puts "Let's play a game!"
 puts "\t3\n\t2\n\t1"
 
-first_player_name = "Finn"
-first_player_health = 60
+first_player_name = "  \n finn \t ".strip
+first_player_health = 12
 
-puts "#{first_player_name}'s health is #{first_player_health * 3}"
-puts "#{first_player_name}'s health is #{first_player_health / 9}"
-puts "#{first_player_name}'s health is #{first_player_health / 9.0}"
+puts "#{first_player_name.upcase} has a health of #{first_player_health}".center(50, '*')
+puts "#{first_player_name.ljust(30, '.')} #{first_player_health} healh"
+puts "#{first_player_health.to_s.reverse.to_i**2}"
