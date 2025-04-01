72ecba8d910917ff615194e2a5cf7e2ca67fb7e9
Tommy Son <tommy.m.son@gmail.com>
2025-04-01 00:20:41 -0700

    Finish 'Making Methods' section

--- pragmatic_studio/flicks/flicks.rb
+++ pragmatic_studio/flicks/flicks.rb
@@ -1,5 +1,16 @@
-person = 'Tommy'
-rank = 10
+def emojis(emoji, number)
+  "#{emoji}" * number
+end
 
-puts "#{person} has a rank of #{rank}"
-puts "Janelle's favorite boyfriend is #{person}.\nShe gives him a #{rank}!"
+def movie_info(title, rank = title.length)
+  stars = emojis("⭐", rank)
+
+  "#{title.capitalize} has a rank of #{rank}: #{stars}"
+end
+
+puts movie_info("goonies", 10)
+puts movie_info("ghostbusters", 9)
+
+movie_title = "rush hour 1"
+
+puts movie_info(movie_title)
--- pragmatic_studio/studio_game/studio_game.rb
+++ pragmatic_studio/studio_game/studio_game.rb
@@ -1,9 +1,7 @@
-puts "Let's play a game!"
-puts "\t3\n\t2\n\t1"
+def say_hello(name:, health: 100)
+  "I'm #{name.capitalize} with a health of #{health}"
+end
 
-first_player_name = "  \n finn \t ".strip
-first_player_health = 12
-
-puts "#{first_player_name.upcase} has a health of #{first_player_health}".center(50, '*')
-puts "#{first_player_name.ljust(30, '.')} #{first_player_health} healh"
-puts "#{first_player_health.to_s.reverse.to_i**2}"
+puts say_hello(name: "tommy", health: 100)
+puts say_hello(name: "janelle")
+puts say_hello(health: 99, name: "chris")
